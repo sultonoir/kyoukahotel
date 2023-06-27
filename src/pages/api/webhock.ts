7eaa2000719d58e1608430ee2f74c4911c668ff8
sultonoir <sultonalidrus5@gmail.com>
2023-06-27 23:08:46 +0700

    try

--- src/pages/api/webhock.ts
+++ src/pages/api/webhock.ts
@@ -1,48 +1,15 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { type NextApiRequest, type NextApiResponse } from "next";
-// import { buffer } from "micro";
-// import type Stripe from "stripe";
-// import { stripe } from "@/lib/stripe";
 
-// const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET_KEY;
-
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
-
-const webhookHandler = async (
-  req: NextApiRequest,
-  res: NextApiResponse
-): Promise<void> => {
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "POST") {
-    // const buf = await buffer(req);
-    // const sig = req.headers["stripe-signature"];
-
-    // let event: Stripe.Event;
-
-    // try {
-    //   event = stripe.webhooks.constructEvent(buf, sig!, endpointSecret!);
-    // } catch (err: unknown) {
-    //   res.status(400).send(`Webhook Error: ${(err as Error).message}`);
-    //   return;
-    // }
-
-    // // Handle the event
-    // console.log(`Unhandled event type ${event.type}`);
-
-    // // Return a 200 response to acknowledge receipt of the event
-    // res.send("Received");
-    const data = await req.body;
+    const data = req.body;
     // Proses data yang diterima sesuai kebutuhan Anda
     // Misalnya, mencetak data yang diterima
     console.log(data);
+    // Tanggapan dapat dikirim kembali ke sumber permintaan jika diperlukan
+    res.status(200).end("OK");
   } else {
-    res.setHeader("Allow", "POST");
-    res.status(405).end("Method Not Allowed");
+    res.status(405).end();
   }
-};
-
-export default webhookHandler;
+}
